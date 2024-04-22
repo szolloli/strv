3d309d57d9af34888a96c1c04d934377e56f71d3
szolloli <szolloli@fit.cvut.cz>
2024-04-22 15:07:47 +0200

    Create color constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,33 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
+  },
+  green: "#22D486",
+  red: "#F40000",
+  black: "#323C46",
+  disabled: "#E4E4EA",
+  text: {
+    black: "#000000",
+    white: "#FFFFFF",
+    lightGray: "#A7A7B9",
+    darkGray: "#72727B",
+  },
+  background: {
+    primary: "#F9F9FB",
+    secondary: "#FFFFFF",
   },
 };
