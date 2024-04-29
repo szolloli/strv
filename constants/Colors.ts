621955ef3f8cf58c1153bfa4301e98b69c8013e4
szolloli <szolloli@fit.cvut.cz>
2024-04-29 22:58:01 +0200

    Update color constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,21 +1,4 @@
-const tintColorLight = "#2f95dc";
-const tintColorDark = "#fff";
-
 export default {
-  light: {
-    text: "#000",
-    background: "#fff",
-    tint: tintColorLight,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: "#fff",
-    background: "#000",
-    tint: tintColorDark,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorDark,
-  },
   green: "#22D486",
   red: "#F40000",
   black: "#323C46",
@@ -30,4 +13,5 @@ export default {
     primary: "#F9F9FB",
     secondary: "#FFFFFF",
   },
+  overlay: "#00000040",
 };
